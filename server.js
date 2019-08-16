65455dfb682cadba6de36d9a8ebb3b8c8bcd4706
Rafix111 <rafaldoranczyk@gmail.com>
2019-08-16 22:21:06 +0200

    ss

--- server.js
+++ server.js
@@ -41,9 +41,12 @@ app.post("/api/questions-pending-send", (req, res) => {
   res.json(succesfullMessage);
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("server is working");
